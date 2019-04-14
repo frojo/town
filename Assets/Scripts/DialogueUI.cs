533ef35c39285cd5b96cffdf9d0e824bd3529209
frojo <mojoz0101@gmail.com>
2019-04-14 14:50:07 -0400

    yarnspinner boilerplate in unity

---
+++ Assets/Scripts/DialogueUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueUI : Yarn.Unity.DialogueUIBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override IEnumerator RunLine(Yarn.Line line) {
+		yield return null;
+
+	}
+
+	public override IEnumerator RunOptions(Yarn.Options optionsCollection, Yarn.OptionChooser optionChooser) {
+		yield return null;
+	}
+
+	public override IEnumerator RunCommand(Yarn.Command command) {
+		yield return null;
+	}
+
+	public override IEnumerator DialogueComplete() {
+		yield return null;
+	}
+}
