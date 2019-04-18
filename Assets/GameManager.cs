83fa19d0fe2d31f5d426cc8b63b8885dd3783250
Francisco Rojo-Ilvento <mojoz0101@gmail.com>
2019-04-18 14:35:03 -0400

    some player settings and other tweaks to make game presentable for incubator

---
+++ Assets/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class GameManager : MonoBehaviour {
+
+    public DialogueRunner dialogue;
+
+    bool dialogueStarted = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!dialogueStarted && Input.anyKeyDown)
+        {
+            dialogueStarted = true;
+            dialogue.StartDialogue();
+        }
+	}
+}
--- Assets/Scripts/DialogueUI.cs
+++ Assets/Scripts/DialogueUI.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class DialogueUI : Yarn.Unity.DialogueUIBehaviour {
-	
-	// UI element that displays lines
+
+    // UI element of the entire displayed dialogue
+    public GameObject dialogueUIFrame;
+
+    // UI element that displays lines
 	public Text displayedText;
 
     // UI element that represents the character currently talking
@@ -18,6 +21,12 @@ public class DialogueUI : Yarn.Unity.DialogueUIBehaviour {
 
     // hacky. probaby want to do this differently later
     public Animator protagAnimator;
+
+    bool lineFullyShown = false;
+    bool newSegment = false;
+    bool charsCounted = false;
+    int numCharsShownSoFar = 0;
+    string currentFullLine;
 
 	public override IEnumerator RunLine(Yarn.Line line) {
 		Debug.Log(line.text);
@@ -43,16 +52,54 @@ public class DialogueUI : Yarn.Unity.DialogueUIBehaviour {
             Debug.Log("Not properly formatted line (character name)");
         }
 
-        string dialogue = nameDialogue[1];
-        
-        // 
-        displayedText.text = dialogue.TrimStart(' ');
+        currentFullLine = nameDialogue[1].TrimStart(' ');
+        displayedText.text = currentFullLine;
+ 
+        // These next few lines of bullshit let the player click through
+        // the same line
+        lineFullyShown = false;
+        newSegment = true;
+        charsCounted = false;
+        numCharsShownSoFar = 0;
+        while (!lineFullyShown)
+        {
+            if (newSegment) {
+                // when we show a new segment, we need to wait a frame to be
+                // able to count the characters displaued
+                newSegment = false;
+                yield return null;
+            } else if (!charsCounted) {
+                int numCharsVisibleNow = displayedText.cachedTextGenerator.characterCountVisible;
+                numCharsShownSoFar += numCharsVisibleNow;
+                charsCounted = true;
+                if (numCharsShownSoFar == currentFullLine.Length) {
+                    lineFullyShown = true;
+                } else { // still haven't shown the full line
+                    // wait for player input to show the next line
+                    while (Input.anyKeyDown == false)
+                    {
+                        Debug.Log("waiting for user input to show rest of line");
+                        yield return null;
+                    }
+                    charsCounted = false;
+                    newSegment = true;
+                    displayedText.text = currentFullLine.Substring(numCharsShownSoFar);
+                }
+            }
+        }
+        // if (displayedText.cachedTextGenerator.characterCountVisible != fullLine.Length)
+        // {
+            // line too long to fully display
+            
+        // }
       
 
         // Wait for any user input
-        while (Input.anyKeyDown == false) {
+        while ( Input.anyKeyDown == false) {
+            Debug.Log("waiting for user input");
             yield return null;
         }
+        Debug.Log("user input!");
         yield return null;
 	}
 
@@ -77,11 +124,13 @@ public class DialogueUI : Yarn.Unity.DialogueUIBehaviour {
     public override IEnumerator DialogueStarted()
     {
         Debug.Log("dialogue started");
+        dialogueUIFrame.SetActive(true);
         yield return null;
     }
 
     public override IEnumerator DialogueComplete() {
         Debug.Log("dialogue completed");
+        dialogueUIFrame.SetActive(false);
         yield return null;
 	}
 }
