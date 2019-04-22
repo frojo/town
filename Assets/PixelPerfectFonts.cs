e318cbd963348a0d9fb5d57941b9882f56762043
Francisco Rojo-Ilvento <mojoz0101@gmail.com>
2019-04-22 10:52:17 -0400

    dialogue UI tweaks

---
+++ Assets/PixelPerfectFonts.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelPerfectFonts : MonoBehaviour {
+
+    public Font[] fonts;
+
+	// Use this for initialization
+	void Start () {
+        foreach (Font font in fonts)
+        {
+            font.material.mainTexture.filterMode = FilterMode.Point;
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
