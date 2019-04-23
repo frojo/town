341ad2bc9391581abbc8b9980abed7ee8e26e4a8
Francisco Rojo-Ilvento <mojoz0101@gmail.com>
2019-04-23 09:51:33 -0400

    new tennis court sprites and rough player movement (no anim)

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float walkSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        // these built in axes have a ramp up to full
+        float v_x = Input.GetAxis("horizontal") * walkSpeed;
+        float v_y = Input.GetAxis("vertical") * walkSpeed;
+
+
+        transform.Translate(v_x, v_y, 0);
+
+
+        //}
+		
+	}
+}
