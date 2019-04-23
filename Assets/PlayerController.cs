221c946254b000c068af275edb6ae67e579fd21c
Francisco Rojo-Ilvento <mojoz0101@gmail.com>
2019-04-23 13:53:27 -0400

    more running anim work

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float walkSpeed;
+    bool facingRight;
+
+    public Animator animator;
+    public SpriteRenderer sprite;
 
 	// Use this for initialization
 	void Start () {
@@ -13,10 +17,18 @@ public class PlayerController : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        // these built in axes have a ramp up to full
-        float v_x = Input.GetAxis("horizontal") * walkSpeed;
-        float v_y = Input.GetAxis("vertical") * walkSpeed;
+        // "raw" means only 1, 0, or -1. no ramping
+        float v_x = Input.GetAxisRaw("horizontal") * walkSpeed;
+        float v_y = Input.GetAxisRaw("vertical") * walkSpeed;
 
-        transform.Translate(v_x, v_y, 0);
+        // nb: we don't change facingRight if v_x == 0
+        if (v_x > 0) facingRight = true;
+        else if (v_x < 0) facingRight = false;
+        sprite.flipX = !facingRight;
+
+        bool moving = v_x != 0 || v_y != 0;
+        animator.SetBool("moving", moving);
+
+        // transform.Translate(v_x, v_y, 0);
 	}
 }
