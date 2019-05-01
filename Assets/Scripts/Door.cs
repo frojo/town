2033b263ef94e4ab34ba7f5419e1fe9c63b3eef6
Francisco Rojo-Ilvento <mojoz0101@gmail.com>
2019-05-01 14:30:24 -0400

    making door work

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    public GameObject open;
+    public GameObject closed;
+
+    public bool isOpen = false;
+
+	// Use this for initialization
+	void Start () {
+		if (isOpen)
+        {
+            Open();
+        } else
+        {
+            Close();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Toggle()
+    {
+        if (isOpen)
+        {
+            Close();
+        } else
+        {
+            Open();
+        }
+    }
+
+    public void Open()
+    {
+        open.SetActive(true);
+        closed.SetActive(false);
+        isOpen = true;
+    }
+
+    public void Close()
+    {
+        closed.SetActive(true);
+        open.SetActive(false);
+        isOpen = false;
+    }
+}
