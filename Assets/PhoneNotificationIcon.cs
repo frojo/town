b693cc2cb09289cc5184283151cb5f5027a71305
Francisco Rojo-Ilvento <mojoz0101@gmail.com>
2019-05-14 12:20:09 -0400

    phone notification event implemented

---
+++ Assets/PhoneNotificationIcon.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhoneNotificationIcon : MonoBehaviour
+{
+    // pretty hacky but:
+    // notification of 1 message
+    public Sprite notification1;
+    // notification of 2 messages
+    public Sprite notification2;
+
+    public Image shownNotification;
+
+    public RectTransform shownEndPosition;
+    public RectTransform hiddenStartPosition;
+    // Transform goalPosition
+
+    // start it hidden
+    public bool showing = false;
+    public float showSpeed = 10;
+    public float hideSpeed = 3;
+    public float notificationDuration = 10;
+
+    // rectTransform of GameObject this script is attached to
+    RectTransform rectTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = transform.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (showing)
+        {
+            rectTransform.anchoredPosition = Vector2.MoveTowards(
+                rectTransform.anchoredPosition,
+                shownEndPosition.anchoredPosition, showSpeed);
+        } else
+        {
+            rectTransform.anchoredPosition = Vector2.MoveTowards(
+                rectTransform.anchoredPosition,
+                hiddenStartPosition.anchoredPosition, hideSpeed);
+        }
+
+        Debug.Log("hidden pos = " + hiddenStartPosition.position.ToString());
+    }
+
+    public void ShowNotification(uint numMessages)
+    {
+        if (numMessages > 2 || numMessages < 1)
+        {
+            // i'll write a better error message later. sue me.
+            Debug.Log("nah");
+            numMessages = 1;
+        }
+
+        // set the correct notification
+        if (numMessages == 1) shownNotification.sprite = notification1;
+        else if (numMessages == 2) shownNotification.sprite = notification2;
+
+        showing = true;
+
+        // todo: set a timer to hide notification after x seconds
+        StartCoroutine(HideAfterSeconds(notificationDuration));
+    }
+
+    private IEnumerator HideAfterSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        showing = false;
+    }
+}
--- Assets/PhoneTextMessage.cs
+++ Assets/PhoneTextMessage.cs
@@ -6,6 +6,7 @@ public class PhoneTextMessage : MonoBehaviour
 {
 
     public PlayerController player;
+    public PhoneNotificationIcon notification;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +19,21 @@ public class PhoneTextMessage : MonoBehaviour
         
     }
 
+    IEnumerator ShowNotifications()
+    {
+        notification.ShowNotification(1);
+        yield return new WaitForSeconds(3);
+        notification.ShowNotification(2);
+    }
+
     void OnTriggerEnter2D(Collider2D o) {
         if (o.CompareTag("Player"))  {
-            // display text message notifiaciton
-            Debug.Log("text message!");
+            // display scripted text message notifications
+            StartCoroutine(ShowNotifications());    
             player.phoneUnlocked = true;
         }
+
     }
+
+    
 }
