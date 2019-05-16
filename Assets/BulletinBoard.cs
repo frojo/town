221ba0ffbef1bcde41e283c53dc1566589c51603
frojo <mojoz0101@gmail.com>
2019-05-16 17:36:51 -0400

    bulletin board half-drawn half-implemented. it's gamebreaking buggy

---
+++ Assets/BulletinBoard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletinBoard : MonoBehaviour
+{
+    public PlayerController player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleActive() {
+
+        // this is hacky and fragile, i know
+        player.inputEnabled = !player.inputEnabled;
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+}
